755c5b6a6cc871c21a2d009216cc3aeb5949f5d7
4116 <4116@CMDLHRLT647>
2022-06-30 06:54:12 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace A2Q12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write a sentence which contains some 'ies' at the end of one or more words: ");
+            string sentence = Console.ReadLine();
+
+            //Printing initial string
+            //Console.WriteLine(sentence);
+
+            //using replace function to change 'ies' to 's'
+            string newSen = sentence.Replace("ies", "s");
+
+            Console.WriteLine("\n\nThe modifield sentence is: ");
+            Console.WriteLine(newSen);
+        }
+    }
+}
